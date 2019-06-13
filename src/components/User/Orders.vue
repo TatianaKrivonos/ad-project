f8d283b79d0b6682500efedbfd1f385ca363719c
TatianaKrivonos <konan_air.96@mail.ru>
2019-06-13 21:51:39 +0300

    создание страницы orders

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,37 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-2">Orders</h1>
+        <v-list
+          subheader
+          two-line
+        >
+          <v-list-tile
+            avavtar
+            v-for="order in orders"
+            :key="order.id"
+            >
+            <v-list-tile-action>
+              <v-checkbox
+                color="info"
+                @change="markDone(order)">
+                :input-value="order.done"
+              </v-checkbox>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ order.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ order.phone }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn
+                class="primary"
+                :to="'/ad/' + order.adId">Open
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +40,19 @@
   export default {
     data () {
       return {
-
+        orders: [
+          {
+            id: '1',
+            name: 'Tanya',
+            phone: '88005353535',
+            adId: '',
+            done: false          }
+        ]
+      }
+    },
+    methods: {
+      markDone (order) {
+        order.done = true
       }
     }
   }
