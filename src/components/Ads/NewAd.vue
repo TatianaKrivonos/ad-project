9d92ce49bbc57344098d16d600b9fc5eaa8b2866
TatianaKrivonos <konan_air.96@mail.ru>
2019-06-13 22:27:58 +0300

    создание страницы формы с новыми объявлениями

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,64 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>NewAd</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Create new ad</h1>
+        <v-form v-model="valid" validation ref="form" class="mb-2">
+          <v-text-field
+            name="title"
+            label="Ad title"
+            type="text"
+            v-model="title"
+            required
+            :rules="[ v => !!v || 'Title is required']"
+          >
+          </v-text-field>
+          <v-textarea
+            name="description"
+            label="Ad descr"
+            type="text"
+            v-model="descr"
+            no-resize
+            :rules="[ v => !!v || 'Description is required']"
+          >
+          </v-textarea>
+        </v-form>
+        <v-layout row class="mb-3">
+          <v-flex xs12>
+            <v-btn
+              class="warning"
+            >
+              Upload
+              <v-icon right dark>cloud_upload</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <img src="" height="150">
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch
+              color="primary"
+              v-model="promo"
+              label="Add to promo"
+            >
+
+            </v-switch>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-spacer></v-spacer>
+            <v-btn
+              :disabled="!valid"
+              class="success"
+              @click="createAd"
+              >Create Ad</v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +67,23 @@
   export default {
     data () {
       return {
+        title: '',
+        descr: '',
+        promo: false,
+        valid: false
+      }
+    },
+    methods: {
+      createAd () {
+        if (this.$refs.form.validate()) {
+          const ad = {
+            title: this.title,
+            descr: this.descr,
+            promo: this.promo
+          }
 
+          console.log(ad)
+        }
       }
     }
   }
