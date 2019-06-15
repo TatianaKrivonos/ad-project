08be6b5118d3f607acb4d2c121ca8b8ecc8f8697
TatianaKrivonos <konan_air.96@mail.ru>
2019-06-15 23:25:49 +0300

    создание страницы my ads

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,34 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>AdList</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">My ads</h1>
+        <v-card
+        class="elevation-10 mb-4"
+        v-for="ad in ads"
+        :key="ad.id">
+          <v-layout row>
+            <v-flex xs4>
+              <v-card-media
+              :src="ad.imgSrc"
+              height="160">
+
+              </v-card-media>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{ ad.title }}</h2>
+                <p>{{ ad.descr }}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  class="info"
+                  :to="'/ad' + ad.id">Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +37,22 @@
   export default {
     data () {
       return {
-
+        ads: [
+          {
+            title: 'First ad',
+            descr: 'hello',
+            promo: false,
+            imgSrc: 'http://wallpapers-images.ru/1280x720/space/wallpapers/wallpapers-space-025.jpg',
+            id: '1'
+          },
+          {
+            title: 'Second ad',
+            descr: 'hello',
+            promo: true,
+            imgSrc: 'http://wallpapers-image.ru/1280x720/winter/wallpapers/winter-wallpapers-1280x720-00012.jpg',
+            id: '2'
+          }
+        ]
       }
     }
   }
