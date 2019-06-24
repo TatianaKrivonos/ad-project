4251215152e36a993f1d34316e87ffbe8a65c5fa
TatianaKrivonos <konan_air.96@mail.ru>
2019-06-24 22:48:28 +0300

    создание модального окна

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <v-card>
+        <v-card v-if="!loading">
           <v-img
             :src="ad.imgSrc"
             height="300">
@@ -13,22 +13,44 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn class="warning" flat>Edit</v-btn>
+            <!-- <v-btn class="warning" flat>Edit</v-btn> -->
+            <app-edit-ad-modal :ad="ad"></app-edit-ad-modal>
             <v-btn class="success">Buy</v-btn>
           </v-card-actions>
         </v-card>
+        <div v-else class="text-xs-center">
+          <v-container>
+            <v-layout row>
+              <v-flex xs12 class="text-xs-center pt-5">
+                <v-progress-circular
+                  :size="100"
+                  color="primary"
+                  indeterminate
+                >
+                </v-progress-circular>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 <script>
+  import EditAdModal from './EditAdModal'
   export default {
     props: ['id'],
     computed: {
       ad () {
         const id = this.id
         return this.$store.getters.adById(id)
+      },
+      loading () {
+        return this.$store.getters.loading
       }
+    },
+    components: {
+      appEditAdModal: EditAdModal
     }
   }
 </script>
