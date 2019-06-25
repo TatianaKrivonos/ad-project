6fd39eff5d60af5c176439c9cc92f456a38ced9a
TatianaKrivonos <konan_air.96@mail.ru>
2019-06-25 23:26:51 +0300

    обработка заказов

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,7 +1,15 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
+      <v-flex xs12 class="text-xs-center pt-5" v-if="loading">
+        <v-progress-circular
+          :size="100"
+          color="primary"
+          indeterminate
+        >
+        </v-progress-circular>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 v-else-if="!loading && orders.length !== 0">
         <h1 class="text--secondary mb-2">Orders</h1>
         <v-list
           subheader
@@ -33,27 +41,29 @@
           </v-list-tile>
         </v-list>
       </v-flex>
+      <v-flex xs12 class="text-xs-center" v-else>
+        <h1 class="text--secondary">You have no orders</h1>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 <script>
   export default {
-    data () {
-      return {
-        orders: [
-          {
-            id: '1',
-            name: 'Tanya',
-            phone: '88005353535',
-            adId: '',
-            done: false          }
-        ]
+    computed: {
+      loading () {
+        return this.$store.getters.loading
+      },
+      orders () {
+        return this.$store.getters.orders
       }
     },
     methods: {
       markDone (order) {
         order.done = true
       }
+    },
+    created () {
+      this.$store.dispatch('fetchOrders')
     }
   }
 </script>
--- src/store/orders.js
+++ src/store/orders.js
@@ -18,16 +18,47 @@ export default {
     }
   },
   actions: {
-   async createOrder ({commit}, {name, phone, adId, ownerId}) {
-    const order = new Order(name, phone, adId)
-    commit('clearError')
-    try {
-      await fb.database().ref(`/users/${ownerId}/orders`).push(order)
-    } catch (error) {
-      commit('setError', error.message)
-      throw error
+    async createOrder ({commit}, {name, phone, adId, ownerId}) {
+      const order = new Order(name, phone, adId)
+      commit('clearError')
+      try {
+        await fb.database().ref(`/users/${ownerId}/orders`).push(order)
+      } catch (error) {
+        commit('setError', error.message)
+        throw error
+      }
+    },
+    async fetchOrders ({commit, getters}) {
+      commit('setLoading', true)
+      commit('clearError')
+      const resultOrders = []
+      try {
+        const fbVal = await fb.database().ref(`/users/${getters.user.id}/orders`).once('value')
+        const orders = fbVal.val()
+
+        Object.keys(orders).forEach(key => {
+          const o = orders[key]
+          resultOrders.push(
+            new Order(o.name, o.phone, o.adId, o.done, key)
+          )
+        })
+        commit('loadOrders', resultOrders)
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setLoading', false)
+        commit('setError', error.message)
+      }
     }
-   }
   },
-  getters: {}
+  getters: {
+    doneOrders (state) {
+      return state.orders.filter(o => o.done)
+    },
+    undoneOrders (state) {
+      return state.orders.filter(o => !o.done)
+    },
+    orders (state, getters) {
+      return getters.undoneOrders.concat(getters.doneOrders)
+    }
+  }
 }
